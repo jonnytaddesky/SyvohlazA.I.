8ade00afdaa338f6324121b3d934fad876af536d
jonnytaddesky <s.v.artem2020@gmail.com>
2019-10-11 09:31:45 +0000

    fixed review issues

--- Homework3/class_Car.py
+++ Homework3/class_Car.py
@@ -7,22 +7,26 @@ class Vehicle(object):
         self.fuelType = fuelType
         self.vehicleType = vehicleType
     
-    def door(self, isopen=False):
-        if isopen:
-            return 'Open'
-        else:
-            return 'Close'
-    
     def fuel(self, isfuel=True):
-        if isfuel:
-            return 'Full Fuel'
+        return 'Full Fuel' if isfuel else 'No Fuel'
+    
+    def door(self, isopen=False):
+        return 'Open' if isopen else 'Close'
+    
+    def brake(self, isbrake=True):
+        if isbrake:
+            return 'Brake a %s %s' % (self.color, self.vehicleType)
         else:
-            return 'No Fuel'
+            return 'Drive a %s %s' % (self.color, self.vehicleType)
+
+    def drive(self, isdrive=False):
+        if isdrive:
+            return 'Drive a %s %s' % (self.color, self.vehicleType)
+        else:
+            return 'Brake a %s %s' % (self.color, self.vehicleType)
 
 
 class Car(Vehicle):
-    isopen = True
-    isfuel = True
     
     def set(self, doors, tires, color, fuelType, vehicleType):
         self.doors = doors
@@ -30,26 +34,9 @@ class Car(Vehicle):
         self.color = color
         self.fuelType = fuelType
         self.vehicleType = vehicleType
-    
-    def brake(self):
-        return 'I`m stop a %s %s' % (self.color, self.vehicleType)
-    
-    def openCloseDoor(self):
-        if not self.isopen:
-            return 'Close Door'
-        else:
-            return 'Open Door'
-    
-    def noFullFuel(self):
-        if not self.isfuel:
-            return 'No Fuel'
-        else:
-            return 'Full Fuel'
 
 
 class Truck(Vehicle):
-    isopen = False
-    isfuel = False
     
     def set(self, doors, tires, color, fuelType, vehicleType, hindcarriage):
         self.doors = doors
@@ -59,21 +46,6 @@ class Truck(Vehicle):
         self.vehicleType = vehicleType
         self.hindcarriage = hindcarriage
     
-    def openCloseDoor(self):
-        if not self.isopen:
-            return 'Close Door'
-        else:
-            return 'Open Door'
-    
-    def noFullFuel(self):
-        if not self.isfuel:
-            return 'No Fuel'
-        else:
-            return 'Full Fuel'
-    
-    def drive(self):
-        return 'I`m driving a %s %s' % (self.color, self.vehicleType)
-
 
 car = Car(5, 4, 'red', 'benzine', 'car')
 print(car.doors, car.tires, car.color, car.fuelType, car.vehicleType)
@@ -82,23 +54,23 @@ truck = Truck(2, 6, 'blue', 'diesel', 'truck')
 print(truck.doors, truck.tires, truck.color, truck.fuelType, truck.vehicleType)
 
 print(car.brake())
+print(car.brake(False))
+
 print(truck.drive())
+print(truck.drive(True))
 
-print('Car: ', car.fuel())
-print('Truck: ', truck.fuel())
+print('Car fuel: ', car.fuel())
+print('Car fuel: ', car.fuel(False))
 
-car.isfuel = False
-print('Car: ', car.noFullFuel())
-truck.isfuel = True
-print('Truck: ', truck.noFullFuel())
+print('Truck fuel: ', truck.fuel())
+print('Truck fuel: ', truck.fuel(False))
 
-car.isopen = True
-print('Car:', car.door())
-truck.isopen = False
-print('Truck:', truck.door())
 
-print('Car:', car.openCloseDoor())
-print('Truck: ', truck.openCloseDoor())
+print('Car door:', car.door())
+print('Car door:', car.door(True))
+
+print('Truck door:', truck.door())
+print('Truck door:', truck.door(True))
 
 car.set(5, 4, 'red', 'benzine', 'car')
 print('Doors:', car.doors, ', tires:', car.tires, ', color:', car.color, ', type fuel:', car.fuelType,
